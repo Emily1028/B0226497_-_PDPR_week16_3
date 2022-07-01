31d578037e2a6101ab31695b17031051ab3d5a15
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-02 00:03:58 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace B0226497_段昱如_PDPR_week16_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.第一張ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.上一張ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.下一張ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.最末張ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 593);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "B0226497_段昱如_PDPR_week16_3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(659, 506);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.第一張ToolStripMenuItem,
+            this.上一張ToolStripMenuItem,
+            this.下一張ToolStripMenuItem,
+            this.最末張ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 100);
+            // 
+            // 第一張ToolStripMenuItem
+            // 
+            this.第一張ToolStripMenuItem.Name = "第一張ToolStripMenuItem";
+            this.第一張ToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.第一張ToolStripMenuItem.Text = "第一張";
+            this.第一張ToolStripMenuItem.Click += new System.EventHandler(this.第一張ToolStripMenuItem_Click);
+            // 
+            // 上一張ToolStripMenuItem
+            // 
+            this.上一張ToolStripMenuItem.Name = "上一張ToolStripMenuItem";
+            this.上一張ToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.上一張ToolStripMenuItem.Text = "上一張";
+            this.上一張ToolStripMenuItem.Click += new System.EventHandler(this.上一張ToolStripMenuItem_Click);
+            // 
+            // 下一張ToolStripMenuItem
+            // 
+            this.下一張ToolStripMenuItem.Name = "下一張ToolStripMenuItem";
+            this.下一張ToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.下一張ToolStripMenuItem.Text = "下一張";
+            this.下一張ToolStripMenuItem.Click += new System.EventHandler(this.下一張ToolStripMenuItem_Click);
+            // 
+            // 最末張ToolStripMenuItem
+            // 
+            this.最末張ToolStripMenuItem.Name = "最末張ToolStripMenuItem";
+            this.最末張ToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.最末張ToolStripMenuItem.Text = "最末張";
+            this.最末張ToolStripMenuItem.Click += new System.EventHandler(this.最末張ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 664);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "B0226497_段昱如_PDPR_week16_3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 第一張ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 上一張ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 下一張ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 最末張ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B0226497_段昱如_PDPR_week16_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int index = 0;
+        List<Bitmap> b = new List<Bitmap>();
+        void showpic()
+        {
+            pictureBox1.Image = b[index];
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                b.Add(new Bitmap($"Week16_{i + 1}.jpg"));
+            }
+            showpic();
+        }
+
+        private void 第一張ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            showpic();
+        }
+
+        private void 上一張ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index--;
+            if (index < 0)
+                index = 0;
+            showpic();
+        }
+
+        private void 下一張ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index++;
+            if (index > 3)
+                index = 3;
+            showpic();
+        }
+
+        private void 最末張ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index=3;
+            showpic();
+        }
+    }
+}
